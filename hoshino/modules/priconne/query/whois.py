251455f152b72e9cc2f9b2af0882db7c1dd47cc9
lostdegree <lostdegreedb@gmail.com>
2020-06-28 18:17:05 +0800

    新功能:查询角色ub语音&帮助更新

--- hoshino/modules/priconne/query/whois.py
+++ hoshino/modules/priconne/query/whois.py
@@ -3,6 +3,9 @@ from hoshino.util import FreqLimiter
 
 from .. import chara
 from . import sv
+from hoshino import R
+import os
+from os import path
 
 lmt = FreqLimiter(5)
 
@@ -35,3 +38,37 @@ async def whois(bot, ev: CQEvent):
     if confi > 60:
         msg += f'{c.icon.cqcode} {c.name}'        
         await bot.send(ev, msg, at_sender=True)
+
+
+
+@sv.on_prefix(('.ub', '。ub'))
+async def ubrec(bot, ev: CQEvent):
+    uid = ev.user_id
+    if not lmt.check(uid):
+        await bot.send(ev, f'您〇太快了，请稍等一会儿(剩余 {int(lmt.left_time(uid)) + 1}秒)', at_sender=True)
+        return
+    lmt.start_cd(uid, 5)
+
+    name = ev.message.extract_plain_text().strip()
+    if not name:
+        await bot.send(ev, '请发送"谁是"+别称，如"谁是霸瞳"')
+        return
+    id_ = chara.name2id(name)
+    confi = 100
+    if id_ == chara.UNKNOWN:
+        id_, guess_name, confi = chara.guess_id(name)
+    c = chara.fromid(id_)
+    
+    msg = ''
+    if confi < 100:
+        lmt.start_cd(uid, 60)
+        msg = f'兰德索尔似乎没有叫"{name}"的人...\n角色别称补全计划: github.com/Ice-Cirno/HoshinoBot/issues/5 \n您有{confi}%的可能在找{guess_name} '
+        await bot.send(ev, msg)
+        #msg = f'\n您有{confi}%的可能在找{guess_name} '
+
+    if confi > 60 and os.path.isfile(f'E:/1data/1data/res/img/priconne/rec/rec_unit_{id_}_ub.mp3'):
+        msg = R.rec(f'priconne/rec/rec_unit_{id_}_ub.mp3').cqcode        
+        await bot.send(ev, msg, at_sender=False)
+
+    else:
+        await bot.send(ev, f'暂时未收录对应语音，该宁来表演了~', at_sender=True)
