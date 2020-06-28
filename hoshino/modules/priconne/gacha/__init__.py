6398e767f809246afdcb6ee09cdd8b03e9f21ba7
lostdegree <lostdegreedb@gmail.com>
2020-06-28 17:48:21 +0800

    gacha增加宝石功能&触发调整

--- hoshino/modules/priconne/gacha/__init__.py
+++ hoshino/modules/priconne/gacha/__init__.py
@@ -1,10 +1,12 @@
 import os
 import random
 from collections import defaultdict
+import re
 
 from hoshino import Service, priv, util
 from hoshino.typing import *
 from hoshino.util import DailyNumberLimiter, concat_pic, pic2b64, silence
+from nonebot import on_natural_language, NLPSession
 
 from .. import chara
 from .gacha import Gacha
@@ -14,20 +16,23 @@ try:
 except:
     import json
 
-
 sv_help = '''
-[星乃来发十连] 转蛋模拟
-[星乃来发单抽] 转蛋模拟
-[星乃来一井] 4w5钻！
+[凯露来发十连] 转蛋模拟
+[凯露来发单抽] 转蛋模拟
+[凯露来一井] 4w5钻！
+[凯露抽签] 随机获得/减少宝石
+[凯露宝石] 查看剩余宝石
 [查看卡池] 模拟卡池&出率
+※宝石用于限制每日单抽/十连模拟的次数
 '''.strip()
 sv = Service('gacha', help_=sv_help, bundle='pcr娱乐')
-jewel_limit = DailyNumberLimiter(6000)
-tenjo_limit = DailyNumberLimiter(1)
+jewel_limit = DailyNumberLimiter(4500)
+tenjo_limit = DailyNumberLimiter(10)
+lmtw = DailyNumberLimiter(1)
 
-JEWEL_EXCEED_NOTICE = f'您今天已经抽过{jewel_limit.max}钻了，欢迎明早5点后再来！'
-TENJO_EXCEED_NOTICE = f'您今天已经抽过{tenjo_limit.max}张天井券了，欢迎明早5点后再来！'
-SWITCH_POOL_TIP = ''
+JEWEL_EXCEED_NOTICE = f'别抽力别抽力，您今天已经抽过{jewel_limit.max}钻了，欢迎明早5点后再来！'
+TENJO_EXCEED_NOTICE = f'真当您是母猪了？您今天已经抽过{tenjo_limit.max}张天井券了，欢迎明早5点后再来！'
+SWITCH_POOL_TIP = '※发送"选择卡池"可切换'
 POOL = ('MIX', 'JP', 'TW', 'BL')
 DEFAULT_POOL = POOL[0]
 
@@ -92,132 +97,206 @@ async def set_pool(bot, ev: CQEvent):
     await gacha_info(bot, ev)
 
 
-async def check_jewel_num(bot, ev: CQEvent):
-    if not jewel_limit.check(ev.user_id):
-        await bot.finish(ev, JEWEL_EXCEED_NOTICE, at_sender=True)
+async def check_jewel_num(session):
+    uid = session.ctx['user_id']
+    if not jewel_limit.check(uid):
+        await session.send(f'别抽力别抽力，您今天已经抽完钻了，欢迎明早5点后再来！', at_sender=True)
+        if not jewel_limit.check(uid):
+            await session.finish(JEWEL_EXCEED_NOTICE)
 
 
-async def check_tenjo_num(bot, ev: CQEvent):
-    if not tenjo_limit.check(ev.user_id):
-        await bot.finish(ev, TENJO_EXCEED_NOTICE, at_sender=True)
+async def check_limit_num(session):
+    uid = session.ctx['user_id']
+    if lmtw.check(uid):
+        await session.send(f'可以发凯露抽签来额外获取宝石哦', at_sender=True)
+        if not lmtw.check(uid):
+            pass
 
 
-@sv.on_prefix(gacha_1_aliases, only_to_me=True)
-async def gacha_1(bot, ev: CQEvent):
-
-    await check_jewel_num(bot, ev)
-    jewel_limit.increase(ev.user_id, 150)
-
-    gid = str(ev.group_id)
-    gacha = Gacha(_group_pool[gid])
-    chara, hiishi = gacha.gacha_one(gacha.up_prob, gacha.s3_prob, gacha.s2_prob)
-    silence_time = hiishi * 60
-
-    res = f'{chara.name} {"★"*chara.star}'
-    if sv.bot.config.USE_CQPRO:
-        res = f'{chara.icon.cqcode} {res}'
-
-    await silence(ev, silence_time)
-    await bot.send(ev, f'素敵な仲間が増えますよ！\n{res}\n{SWITCH_POOL_TIP}', at_sender=True)
+async def check_tenjo_num(session):
+    uid = session.ctx['user_id']
+    if not tenjo_limit.check(uid):
+        await session.send(TENJO_EXCEED_NOTICE, at_sender=True)
+        if not tenjo_limit.check(uid):
+            await session.finish(TENJO_EXCEED_NOTICE)
 
 
-@sv.on_prefix(gacha_10_aliases, only_to_me=True)
-async def gacha_10(bot, ev: CQEvent):
-    SUPER_LUCKY_LINE = 170
+@sv.on_natural_language(keywords={'臭鼬', '猫猫', '凯露'}, only_to_me=False)
+async def gacha_st(session:NLPSession):
 
-    await check_jewel_num(bot, ev)
-    jewel_limit.increase(ev.user_id, 1500)
-
-    gid = str(ev.group_id)
-    gacha = Gacha(_group_pool[gid])
-    result, hiishi = gacha.gacha_ten()
-    silence_time = hiishi * 6 if hiishi < SUPER_LUCKY_LINE else hiishi * 60
-
-    if sv.bot.config.USE_CQPRO:
-        res1 = chara.gen_team_pic(result[:5], star_slot_verbose=False)
-        res2 = chara.gen_team_pic(result[5:], star_slot_verbose=False)
-        res = concat_pic([res1, res2])
-        res = pic2b64(res)
-        res = MessageSegment.image(res)
-        result = [f'{c.name}{"★"*c.star}' for c in result]
-        res1 = ' '.join(result[0:5])
-        res2 = ' '.join(result[5:])
-        res = f'{res}\n{res1}\n{res2}'
-    else:
-        result = [f'{c.name}{"★"*c.star}' for c in result]
-        res1 = ' '.join(result[0:5])
-        res2 = ' '.join(result[5:])
-        res = f'{res1}\n{res2}'
-
-    if hiishi >= SUPER_LUCKY_LINE:
-        await bot.send(ev, '恭喜海豹！おめでとうございます！')
-    await bot.send(ev, f'素敵な仲間が増えますよ！\n{res}\n{SWITCH_POOL_TIP}', at_sender=True)
-    await silence(ev, silence_time)
+    arg = session.msg_text.strip()
+    rex = re.compile(r'.*(宝石|石头|钻|水).*')
+    m = rex.search(arg)
+    if m:
+        uid = session.ctx['user_id']
+        num = jewel_limit.get_num(uid)
+        await session.send(f'现在有{4500 - num}宝石')
 
 
-@sv.on_prefix(gacha_300_aliases, only_to_me=True)
-async def gacha_300(bot, ev: CQEvent):
+@sv.on_natural_language(keywords={'臭鼬', '猫猫', '凯露'}, only_to_me=False)
+async def gacha_d(session:NLPSession):
 
-    await check_tenjo_num(bot, ev)
-    tenjo_limit.increase(ev.user_id)
-
-    gid = str(ev.group_id)
-    gacha = Gacha(_group_pool[gid])
-    result = gacha.gacha_tenjou()
-    up = len(result['up'])
-    s3 = len(result['s3'])
-    s2 = len(result['s2'])
-    s1 = len(result['s1'])
-
-    res = [*(result['up']), *(result['s3'])]
-    random.shuffle(res)
-    lenth = len(res)
-    if lenth <= 0:
-        res = "竟...竟然没有3★？！"
-    else:
-        step = 4
-        pics = []
-        for i in range(0, lenth, step):
-            j = min(lenth, i + step)
-            pics.append(chara.gen_team_pic(res[i:j], star_slot_verbose=False))
-        res = concat_pic(pics)
-        res = pic2b64(res)
-        res = MessageSegment.image(res)
-
-    msg = [
-        f"\n素敵な仲間が増えますよ！ {res}",
-        f"★★★×{up+s3} ★★×{s2} ★×{s1}",
-        f"获得记忆碎片×{100*up}与女神秘石×{50*(up+s3) + 10*s2 + s1}！\n第{result['first_up_pos']}抽首次获得up角色" if up else f"获得女神秘石{50*(up+s3) + 10*s2 + s1}个！"
-    ]
-
-    if up == 0 and s3 == 0:
-        msg.append("太惨了，咱们还是退款删游吧...")
-    elif up == 0 and s3 > 7:
-        msg.append("up呢？我的up呢？")
-    elif up == 0 and s3 <= 3:
-        msg.append("这位酋长，梦幻包考虑一下？")
-    elif up == 0:
-        msg.append("据说天井的概率只有12.16%")
-    elif up <= 2:
-        if result['first_up_pos'] < 50:
-            msg.append("你的喜悦我收到了，滚去喂鲨鱼吧！")
-        elif result['first_up_pos'] < 100:
-            msg.append("已经可以了，您已经很欧了")
-        elif result['first_up_pos'] > 290:
-            msg.append("标 准 结 局")
-        elif result['first_up_pos'] > 250:
-            msg.append("补井还是不补井，这是一个问题...")
+    arg = session.msg_text.strip()
+    rex = re.compile(r'.*抽签.*')
+    m = rex.search(arg)
+    if m:
+        await check_jewel_num(session)
+        uid = session.ctx['user_id']
+        num = jewel_limit.get_num(uid)
+        if not lmtw.check(uid):
+                await session.send(f'你今天已经抽过签了，现在有{4500 - num}宝石')
+                return
+        lmtw.increase(uid)
+        if random.random() <= 0.9:
+            add_jewel = random.randrange(150,4500,150)
+            jewel_limit.decrease(uid, add_jewel)
+            await session.send(f'奇怪的宝石增加了！\n获得{add_jewel}宝石')
         else:
-            msg.append("期望之内，亚洲水平")
-    elif up == 3:
-        msg.append("抽井母五一气呵成！多出30等专武～")
-    elif up >= 4:
-        msg.append("记忆碎片一大堆！您是托吧？")
-    msg.append(SWITCH_POOL_TIP)
+            jewel_limit.increase(uid, 4350)
+            await session.send('宝石被猫猫偷走了 >_< 就↗剩↘150宝石了')
 
-    await bot.send(ev, '\n'.join(msg), at_sender=True)
-    silence_time = (100*up + 50*(up+s3) + 10*s2 + s1) * 1
-    await silence(ev, silence_time)
+
+@sv.on_natural_language(keywords={'臭鼬', '猫猫', '凯露'}, only_to_me=False)
+async def gacha_1(session:NLPSession):
+
+    arg = session.msg_text.strip()
+    rex = re.compile(r'^(?!.*(十|10|井)).*(来|抽).*(一|1).*(发|抽)|.*单.*(发|抽)')
+    m = rex.search(arg)
+    if m:
+        await check_limit_num(session)
+    if m:
+        await check_jewel_num(session)
+        uid = session.ctx['user_id']
+        jewel_limit.increase(uid, 150)
+
+        gid = str(session.ctx['group_id'])
+        gacha = Gacha(_group_pool[gid])
+        chara, hiishi = gacha.gacha_one(gacha.up_prob, gacha.s3_prob, gacha.s2_prob)
+        silence_time = hiishi * 60
+
+        res = f'{chara.name} {"★"*chara.star}'
+        if sv.bot.config.USE_CQPRO:
+            res = f'{chara.icon.cqcode} {res}'
+
+        #await silence(session.ctx, silence_time)
+        await session.send(f'素敵な仲間が増えますよ！\n{res}\n{SWITCH_POOL_TIP}\n※发送“凯露宝石”可以查看还剩多少宝石', at_sender=True)
+
+
+@sv.on_natural_language(keywords={'臭鼬', '猫猫', '凯露'}, only_to_me=False)
+async def gacha_10(session:NLPSession):
+
+    arg = session.msg_text.strip()
+    rex = re.compile(r'.*(十|10)(次|发|抽|连|下|連).*')
+    m = rex.search(arg)
+    if m:
+        await check_limit_num(session)
+    if m:
+        SUPER_LUCKY_LINE = 170
+
+        await check_jewel_num(session)
+        uid = session.ctx['user_id']
+        jewel_limit.increase(uid, 1500)
+
+        gid = str(session.ctx['group_id'])
+        gacha = Gacha(_group_pool[gid])
+        result, hiishi = gacha.gacha_ten()
+        silence_time = hiishi * 0 if hiishi < SUPER_LUCKY_LINE else hiishi * 6
+
+        if sv.bot.config.USE_CQPRO:
+            res1 = chara.gen_team_pic(result[:5], star_slot_verbose=False)
+            res2 = chara.gen_team_pic(result[5:], star_slot_verbose=False)
+            res = concat_pic([res1, res2])
+            res = pic2b64(res)
+            res = MessageSegment.image(res)
+            result = [f'{c.name}{"★"*c.star}' for c in result]
+            res1 = ' '.join(result[0:5])
+            res2 = ' '.join(result[5:])
+            res = f'{res}\n{res1}\n{res2}'
+        else:
+            result = [f'{c.name}{"★"*c.star}' for c in result]
+            res1 = ' '.join(result[0:5])
+            res2 = ' '.join(result[5:])
+            res = f'{res1}\n{res2}'
+
+        if hiishi >= SUPER_LUCKY_LINE:
+            await session.send('恭喜海豹！おめでとうございます！')
+        await session.send(f'素敵な仲間が増えますよ！\n{res}\n{SWITCH_POOL_TIP}\n※发送“凯露宝石”可以查看还剩多少宝石', at_sender=True)
+        await silence(session.ctx, silence_time)
+
+
+
+@sv.on_natural_language(keywords={'井'}, only_to_me=False)
+async def gacha_300(session:CommandSession):
+
+    arg = session.msg_text.strip()
+    rex = re.compile(r'(.*(臭鼬|猫猫|凯露).*(来|抽|扭蛋|轉蛋).*)|(.*(来|抽|扭蛋|轉蛋).*(臭鼬|猫猫|凯露).*)')
+    m = rex.search(arg)
+    if m:
+        await check_tenjo_num(session)
+        uid = session.ctx['user_id']
+        tenjo_limit.increase(uid)
+
+        gid = str(session.ctx['group_id'])
+        gacha = Gacha(_group_pool[gid])
+        result = gacha.gacha_tenjou()
+        up = len(result['up'])
+        s3 = len(result['s3'])
+        s2 = len(result['s2'])
+        s1 = len(result['s1'])
+
+        res = [*(result['up']), *(result['s3'])]
+        random.shuffle(res)
+        lenth = len(res)
+        if lenth <= 0:
+            res = "竟...竟然没有3★？！"
+        else:
+            step = 4
+            pics = []
+            for i in range(0, lenth, step):
+                j = min(lenth, i + step)
+                pics.append(chara.gen_team_pic(res[i:j], star_slot_verbose=False))
+            res = concat_pic(pics)
+            res = pic2b64(res)
+            res = MessageSegment.image(res)
+
+        msg = [
+            f"\n素敵な仲間が増えますよ！ {res}",
+            f"★★★×{up+s3} ★★×{s2} ★×{s1}",
+            f"获得记忆碎片×{100*up}与女神秘石×{50*(up+s3) + 10*s2 + s1}！\n第{result['first_up_pos']}抽首次获得up角色" if up else f"获得女神秘石{50*(up+s3) + 10*s2 + s1}个！"
+        ]
+
+        if up == 0 and s3 == 0:
+            msg.append("太惨了，咱们还是退款删游吧...")
+        elif up == 0 and s3 > 7:
+            msg.append("up呢？我的up呢？")
+        elif up == 0 and s3 <= 3:
+            msg.append("这位酋长，梦幻包考虑一下？")
+        elif up == 0:
+            msg.append("据说天井的概率只有12.16%")
+        elif up <= 2:
+            if result['first_up_pos'] < 50:
+                msg.append("你的喜悦我收到了，滚去喂鲨鱼吧！")
+            elif result['first_up_pos'] < 100:
+                msg.append("已经可以了，您已经很欧了")
+            elif result['first_up_pos'] > 290:
+                msg.append("标 准 结 局")
+            elif result['first_up_pos'] > 250:
+                msg.append("补井还是不补井，这是一个问题...")
+            else:
+                msg.append("期望之内，亚洲水平")
+        elif up == 3:
+            msg.append("抽井母五一气呵成！多出30等专武～")
+        elif up >= 4:
+            msg.append("记忆碎片一大堆！您是托吧？")
+        msg.append(SWITCH_POOL_TIP)
+
+        if uid == 3106316068:
+            await session.send('emmmm由衣今天又偷懒了啊~', at_sender=False)
+
+        await session.send('\n'.join(msg), at_sender=True)
+        silence_time = (10*up + s2)
+        await silence(session.ctx, silence_time)
+
 
 
 @sv.on_prefix('氪金')
